2329ffb1173b443be494204fae5da5322d0e40af
Toufiq Islam Swoad <toufiq11swoad@gmail.com>
2024-02-18 21:02:02 +0600

    search inputfield problem fixed

--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
     <nav className="bg-base-200">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-          <img src={logo} className="h-8" alt="Flowbite Logo" />
+          <img src={logo} className="h-8" alt="Logo" />
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
             TechShop
           </span>
@@ -43,6 +43,35 @@ const Navbar = () => {
             </svg>
             <span className="sr-only">Search</span>
           </button>
+          <div className="relative hidden md:block">
+            <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+              <svg
+                className="w-4 h-4 text-gray-500 dark:text-gray-400"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 20 20"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                />
+              </svg>
+              <span className="sr-only">Search icon</span>
+            </div>
+            <input
+              type="text"
+              id="search-navbar"
+              className="block w-full p-2 ps-10 text-sm text-gray-900 border
+         border-gray-300 rounded-lg bg-gray-50 focus:ring-gray-200 focus:border-blue-500
+          dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white
+           dark:focus:ring-gray-200 dark:focus:border-blue-500"
+              placeholder="Search..."
+            />
+          </div>
           <button
             type="button"
             onClick={toggleMobileMenu}
@@ -78,7 +107,7 @@ const Navbar = () => {
           <div className="relative mt-3 md:hidden">
             <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
               <svg
-                className="w-4 h-4 text-gray-500 dark:text-gray-400"
+                className="w-4 h-4 text-gray-500"
                 aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
@@ -104,8 +133,8 @@ const Navbar = () => {
             />
           </div>
           <ul
-            className="flex flex-col p-2 md:p-2 pt-2 font-medium border md:space-x-8
-           rtl:space-x-reverse md:flex-row md:mt-0 md:border-0"
+            className="flex flex-col p-2 md:p-2 pt-2 font-medium md:space-x-8
+           rtl:space-x-reverse md:flex-row md:mt-0"
           >
             <li>
               <Link
